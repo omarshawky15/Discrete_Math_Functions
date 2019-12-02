f7c63bfcf422239a8f535d3a550f1e8a8f1684d8
omarshawky15 <47141484+omarshawky15@users.noreply.github.com>
2019-12-02 17:50:41 +0200

    Finished But not fully tested

--- Discrete_Math/src/DMF.java
+++ Discrete_Math/src/DMF.java
@@ -2,10 +2,10 @@ import java.util.ArrayList;
 import java.util.Stack;
 
 public class DMF {
-	ArrayList<Integer> primes  = new ArrayList<Integer>(); 
-	Boolean[] b = new Boolean[(int)10e6+1] ;
+	ArrayList<Integer> primes = new ArrayList<Integer>();
+	Boolean[] b = new Boolean[(int) 10e6 + 1];
 
-	//Overflows 
+	// Overflows
 	public long fastExpN1(long a, long b, long m) {
 		long c = 1;
 		for (long i = 1; i <= b; i++) {
@@ -59,44 +59,97 @@ public class DMF {
 
 		return ans;
 	}
-	public void EGCD (long a , long b) {
-		long aTemp = Math.max(a,b),bTemp = Math.min(b,a);
-		a= aTemp; b= bTemp;
-		long s1 =1 ,s2=0 ,t1=0 , t2=1 , temp =0 ;
-		long q =a/b, r=a%b ,d =r ;
-		a=b ; b= r ;
-		while(r!=0) {
-			temp = s2 ;s2 = s1 - s2*q ; s1 = temp;
-			temp = t2 ; t2=t1-t2*q ; t1 = temp;
-			d=r;
-			r=a%b; q= a/b ;
-			a=b ; b=r;
+
+	public long[] EGCD(long a, long b) {
+		long aTemp = Math.max(a, b), bTemp = Math.min(b, a);
+		a = aTemp;
+		b = bTemp;
+		long s1 = 1, s2 = 0, t1 = 0, t2 = 1, temp = 0;
+		long q = a / b, r = a % b, d = r;
+		a = b;
+		b = r;
+		while (r != 0) {
+			temp = s2;
+			s2 = s1 - s2 * q;
+			s1 = temp;
+			temp = t2;
+			t2 = t1 - t2 * q;
+			t1 = temp;
+			d = r;
+			r = a % b;
+			q = a / b;
+			a = b;
+			b = r;
 		}
-		System.out.println(d + " = ("+ aTemp+"*"+s2+") + (" + bTemp+"*"+t2+")");
+		long[] ans = { d, s2, t2 };
+		return ans;
 	}
-	public void Seive (int n) {
-		if (n<=1||n>(int)(10e6))return ;
-		for(int i=2 ;i<=n ;i++)b[i] = false; 
-		for(int i=2 ; i*i<=n; i++) {
-			if(!b[i]) {
-				for(int j=i*i ; j<=n ;j+=i) {
+
+	public void Seive(int n) {
+		if (n <= 1 || n > (int) (10e6))
+			return;
+		for (int i = 2; i <= n; i++)
+			b[i] = false;
+		for (int i = 2; i * i <= n; i++) {
+			if (!b[i]) {
+				for (int j = i * i; j <= n; j += i) {
 					b[j] = true;
 				}
-			primes.add(i);
+				primes.add(i);
 			}
 		}
 	}
-	public void primeNumberGen() {
-		if(primes.size()==0)Seive((int)10e6);
-		int random = (int)Math.round((primes.size())*Math.random());
-		System.out.println("Your Random number is "+primes.get(random));
+
+	public int primeNumberGen() {
+		if (primes.size() == 0)
+			Seive((int) 10e6);
+		int random = (int) Math.round((primes.size()) * Math.random());
+		// System.out.println("Your Random number is "+primes.get(random));
+		return primes.get(random);
+	}
+
+	public long[] CRT (long [][] par) {
+		long M =1 ,x=0;
+				int sz = par.length;
+		long [][] ans = new long[par.length][3];
+		for(int i =0 ;i<sz; i++) {
+			M*=par[i][1];
+		}
+		for(int i =0 ;i<sz; i++) {
+			long mi  = par[i][1];
+			ans[i][0]   = par[i][0];
+			ans[i][1] = M/par[i][1];
+			ans[i][2] = EGCD(ans[i][1]%mi, mi)[2];
+			x+= (ans[i][0]*ans[i][1]*ans[i][2])%M;
+		}
+		while (x<0)x+=M;
+		//System.out.println("Answer is "+ x + " (mod "+M+")");
+		return new long[] {x,M};
 		
 	}
+
 	public static void main(String[] args) {
 		DMF e = new DMF();
-		System.out.println(e.fastExpIte(3185, 2753, 3233));
-		e.EGCD(21, 44);
-		e.primeNumberGen();
+		long []ans;
+		//Fast Expontiation (Iterative)
+		System.out.println(e.fastExpIte(3185, 2753, 3233) + " From Iterative");	
+		
+		//Fast Expontiation (Recursive)
+		System.out.println(e.fastExpRec(3185, 2753, 3233) + " From Recursive");	
+		
+		//Random Number Generation
+		System.out.println("Your Random number is " + e.primeNumberGen());
+		
+		//Extended Euclidian Theorem 
+		ans = e.EGCD(21, 44);
+		System.out.println(ans[0] + " = (" + 44 + "*" + ans[1] + ") + (" + 21 + "*" + ans[2] + ")");
+
+		//Chinese Reminder Theroem
+		long [][] test = {{2 , 3},{1,4},{3,5}};
+		ans = e.CRT(test);
+		System.out.println("Answer is "+ ans[0] + " (mod "+ans[1]+")");
+
+		
 	}
 
 }
